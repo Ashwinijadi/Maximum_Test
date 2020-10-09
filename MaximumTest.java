4348dac4f67b84939cfbe0ea7bd18b1035540bf4
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-10-09 09:03:21 +0530

    added:To find maximum integer is added with maximum at first position

--- MaximumTest.java
+++ MaximumTest.java
@@ -2,7 +2,18 @@ package com.capgemini.maximum_test;
 
 public class MaximumTest {
 
+	public static int max(int x, int y, int z) {
+		int max = x;
+		if (y > max)
+			max = y;
+		if (z > max)
+			max = z;
+		return max;
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to the Maximum Problems Using Generics");
+		int a = 5, b = 4, c = 3;
+
+		System.out.println("maximum is " + max(a, b, c));
 	}
 }
