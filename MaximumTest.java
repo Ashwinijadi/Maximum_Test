63565942a05302d594c2b3d711e0646088b7e505
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-10-09 10:34:53 +0530

    added: To find maximum Generic Method is added

--- MaximumTest.java
+++ MaximumTest.java
@@ -1,18 +1,51 @@
 package com.capgemini.maximum_test;
 
-public class MaximumTest {
+	public class MaximumTest<T extends Comparable<T>> {
+		T x, y, z;
 
-	public static String max(String x, String y, String z) {
-		String max = x;
-		if (y.compareTo(max) > 0)
-			max = y;
-		if (z.compareTo(max) > 0)
-			max = z;
-		return max;
-	}
+		public MaximumTest(T x, T y, T z) {
+			this.x = x;
+			this.y = y;
+			this.z = z;
+		}
 
-	public static void main(String[] args) {
-		String str1="Apple",str2="Banana",str3="Peach";
-		System.out.println("maximum is " + max(str1,str2,str3));
+		public T maximum() {
+			return MaximumTest.maximum(x, y, z);
+		}
+
+		public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
+			T max = x;
+			if (y.compareTo(max) > 0) {
+				max = y;
+			}
+			if (z.compareTo(max) > 0) {
+				max = z;
+			}
+			printMax(x, y, z, max);
+			return max;
+		}
+		
+		/*public static String maximumTest(String x,  String y, String z) {
+			String max = x;
+			if (y.compareTo(max) > 0) {
+				max = y;
+			}
+			if (z.compareTo(max) > 0) {
+				max = z;
+			}
+			printMax(x, y, z, max);
+			return max;
+		}*/
+		public static <T> void printMax(T x, T y, T z, T max) {
+		System.out.println("maximum is "+max);
+		}
+
+		public static void main(String[] args) {
+			Integer xI = 3, yI = 4, zI = 5;
+			float xF=3.3f,yF=4.4f,zF=5.5f;
+			String xStr="Peach",yStr="Banana",zStr="Apple";
+			MaximumTest.maximum(xI, yI, zI);
+			MaximumTest.maximum(xStr, yStr, zStr);
+			MaximumTest.maximum(xF, yF, zF);
+		}
 	}
-}
