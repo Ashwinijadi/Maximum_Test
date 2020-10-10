6c994b96107d3f2d275d9c1d280fef6a76c0986e
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-10-10 20:23:39 +0530

    Modified:finding maximum functionality is modified for extending more than three parameters

---
+++ ExtendParameters_MaximumTest.java
@@ -0,0 +1,47 @@
+package com.capgemini.maximum_test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ExtendParameters_MaximumTest {
+
+	MaximumTest userInt = new MaximumTest<Integer>();
+	MaximumTest userF = new MaximumTest<Float>();
+	MaximumTest userStr = new MaximumTest<String>();
+
+	@Test
+	public void testIntegerMaximumWithFour_args() {
+		userInt.Maximum(20, 40, 60, 80);
+		Assert.assertEquals(80, userInt.printMax());
+	}
+
+	@Test
+	public void testIntegerMaximumWithFive_args() {
+		userInt.Maximum(20, 100, 40, 60, 80);
+		Assert.assertEquals(100, userInt.printMax());
+	}
+
+	@Test
+	public void testFloatMaximumwithFour_args() {
+		userF.Maximum(20.2f, 80.8f, 40.4f, 60.6f);
+		Assert.assertEquals(80.8f, userF.printMax());
+	}
+
+	@Test
+	public void testFloatMaximumWithFive_args() {
+		userInt.Maximum(20.2f, 100.1f, 40.4f, 60.6f, 80.8f);
+		Assert.assertEquals(100.1f, userInt.printMax());
+	}
+
+	@Test
+	public void testStringMaximum() {
+		userStr.Maximum("Apple", "Peach", "Banana", "Orange");
+		Assert.assertEquals("Peach", userStr.printMax());
+	}
+
+	@Test
+	public void testStringMaximumWithFive_args() {
+		userStr.Maximum("Apple", "Peach", "Banana", "Orange", "Mango");
+		Assert.assertEquals("Peach", userStr.printMax());
+	}
+}
--- MaximumTest.java
+++ MaximumTest.java
@@ -1,45 +1,29 @@
 package com.capgemini.maximum_test;
 
+import java.util.Arrays;
 import java.util.Scanner;
-	
-public class MaximumTest<E extends Comparable<E>> {
-E x,y,z,max;
 
-public  MaximumTest(E x, E y, E z) {
-	this.x = x;
-	this.y = y;
-	this.z = z;
-}
+public class MaximumTest<E extends Comparable<E>> {
+	E x, y, z;
+	E max;
 	public static Scanner input = new Scanner(System.in);
 
-	
+	public MaximumTest() {
 
-	public E maximum() {
-		return MaximumTest.maximum(x, y, z);
 	}
 
-	public static <E extends Comparable<E>> E maximum(E x, E y, E z) {
-		E max = x;
-		if (y.compareTo(max) > 0) {
-			max = y;
-		}
-		if (z.compareTo(max) > 0) {
-			max = z;
-		}
-		printMax(x, y, z, max);
+	public E printMax() {
+		return this.max;
+	}
+
+	public E Maximum(E ...args) {
+		Arrays.sort(args);
+		this.max = args[args.length - 1];
 		return max;
 	}
 
-	public static <T> void printMax(T x, T y, T z, T max) {
-	System.out.printf("maximum of %s,%s and %s is %s\n",x,y,z,max);
+	public static void main(String[] args) {
+		System.out.println("Maximum is found");
 	}
 
-	public static void main(String[] args) {
-		Integer xI = 3, yI = 4, zI = 5;
-		float xF=3.3f,yF=4.4f,zF=5.5f;
-		String xStr="Peach",yStr="Banana",zStr="Apple";
-		MaximumTest.maximum(xI, yI, zI);
-		MaximumTest.maximum(xStr, yStr, zStr);
-		MaximumTest.maximum(xF, yF, zF);
-	}
 }
