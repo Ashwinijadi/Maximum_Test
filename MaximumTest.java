054d717e19a0ec5211ce72fefb1df6d260b12126
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-10-10 20:06:54 +0530

    Refactored: Refactored finding maximum by using Generic class with three variables of generic type

--- MaximumTest.java
+++ MaximumTest.java
@@ -1,51 +1,45 @@
 package com.capgemini.maximum_test;
 
-	public class MaximumTest<T extends Comparable<T>> {
-		T x, y, z;
+import java.util.Scanner;
+	
+public class MaximumTest<E extends Comparable<E>> {
+E x,y,z,max;
 
-		public MaximumTest(T x, T y, T z) {
-			this.x = x;
-			this.y = y;
-			this.z = z;
-		}
+public  MaximumTest(E x, E y, E z) {
+	this.x = x;
+	this.y = y;
+	this.z = z;
+}
+	public static Scanner input = new Scanner(System.in);
 
-		public T maximum() {
-			return MaximumTest.maximum(x, y, z);
-		}
+	
 
-		public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
-			T max = x;
-			if (y.compareTo(max) > 0) {
-				max = y;
-			}
-			if (z.compareTo(max) > 0) {
-				max = z;
-			}
-			printMax(x, y, z, max);
-			return max;
-		}
-		
-		/*public static String maximumTest(String x,  String y, String z) {
-			String max = x;
-			if (y.compareTo(max) > 0) {
-				max = y;
-			}
-			if (z.compareTo(max) > 0) {
-				max = z;
-			}
-			printMax(x, y, z, max);
-			return max;
-		}*/
-		public static <T> void printMax(T x, T y, T z, T max) {
-		System.out.println("maximum is "+max);
-		}
-
-		public static void main(String[] args) {
-			Integer xI = 3, yI = 4, zI = 5;
-			float xF=3.3f,yF=4.4f,zF=5.5f;
-			String xStr="Peach",yStr="Banana",zStr="Apple";
-			MaximumTest.maximum(xI, yI, zI);
-			MaximumTest.maximum(xStr, yStr, zStr);
-			MaximumTest.maximum(xF, yF, zF);
-		}
+	public E maximum() {
+		return MaximumTest.maximum(x, y, z);
 	}
+
+	public static <E extends Comparable<E>> E maximum(E x, E y, E z) {
+		E max = x;
+		if (y.compareTo(max) > 0) {
+			max = y;
+		}
+		if (z.compareTo(max) > 0) {
+			max = z;
+		}
+		printMax(x, y, z, max);
+		return max;
+	}
+
+	public static <T> void printMax(T x, T y, T z, T max) {
+	System.out.printf("maximum of %s,%s and %s is %s\n",x,y,z,max);
+	}
+
+	public static void main(String[] args) {
+		Integer xI = 3, yI = 4, zI = 5;
+		float xF=3.3f,yF=4.4f,zF=5.5f;
+		String xStr="Peach",yStr="Banana",zStr="Apple";
+		MaximumTest.maximum(xI, yI, zI);
+		MaximumTest.maximum(xStr, yStr, zStr);
+		MaximumTest.maximum(xF, yF, zF);
+	}
+}
